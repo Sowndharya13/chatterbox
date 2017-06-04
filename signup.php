ee47a32ccf2b1ee353b5a51862f2f32bcafabfe3
Sowndharya13 <sowndharyakanakaraj@gmail.com>
2017-06-04 17:29:35 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -19,11 +19,15 @@ $user = "root";
     die( 'Connected to Server, but Failed to Connect to Database - #'.mysql_errno().': '.mysql_error());
 
     $selectSQL = " INSERT INTO `users`(`user_id`, `pass`, `email`, `phone`) VALUES ('$u_id', '$pass', '$mail' , '$phone') ";
-  
+   $res=mysql_fetch_row($selectSQL);
+   if($res)
+   {
+    $_SESSION['userid']=$u_id;
+	
+    header('location:afterlogin.php');
+   }
   if( !( $selectRes = mysql_query( $selectSQL ) ) ){
     echo 'Insertion into the Database Failed - #'.mysql_errno().': '.mysql_error();
-  }else{
-  echo 'Account created successffuly';
   }
   
-?><br>Login and continue<a href="portspritle.html">Login and continue</a> <br>
+?><br>Login and continue<a href="index.html">Login and continue</a> <br>
